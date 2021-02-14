b3fd8ea9ae5043e866f5d685aa0d7864b2f7c3eb
YntyCtrl <yntymak.199848@gmai.com>
2021-02-14 13:05:03 +0600

    by test button and randomMath

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
-let password = document.querySelector('#password')
-let pdiv = document.querySelector('#pdiv')
-let bad = "Bad"
-let few = "Few"
+let password = document.querySelector('#password');
+let pdiv = document.querySelector('#pdiv');
+let bad = "Bad";
+let few = "Few";
 
 
 
 
 
 
-password.addEventListener('input', function(event) { // при изменении поля (потеря фокуса)
-    if (password.value.length <= 4){
-        pdiv.innerText =  bad;
-        
+password.addEventListener('input', function (event) { // при изменении поля (потеря фокуса)
+    if (password.value.length <= 4) {
+        pdiv.innerText = bad;
+
     }
-    else if ( password.value.length < 8 ){
-        pdiv.innerText = few ;
-        
+    else if (password.value.length < 8) {
+        pdiv.innerText = few;
+
     }
-    else{
+    else {
         pdiv.innerText = "Good";
     }
-    });
+});
+
+
+let randomdiv = document.querySelector('#randomdiv');
+
+button.addEventListener('click', function (event) {
+
+    let num = Math.ceil(Math.random() * 5);
+
+    if (num == 4){
+        randomdiv.innerText = "4";
+    }
+
+
+    
+
+
+})
 
 
 
- 
 
 
